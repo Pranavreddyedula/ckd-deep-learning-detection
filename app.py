f5ee69410c82ecd5a61e29602897c17b4a5ffbcf
Sai Pranavreddy <edulapranav@gmail.com>
2025-12-18 08:54:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+from tensorflow.keras.models import load_model
+
+
+app = Flask(__name__)
+
+
+model = load_model("models/ckd_dl_model.h5")
+scaler = joblib.load("models/scaler.pkl")
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+if request.method == 'POST':
+values = [float(request.form[f]) for f in request.form]
+data = scaler.transform([values])
+pred = model.predict(data)[0][0]
+
+
+if pred > 0.5:
+result = "Chronic Kidney Disease Detected"
+image = "ckd_kidney.png"
+else:
+result = "Healthy Kidney"
+image = "healthy_kidney.png"
+
+
+return render_template('result.html', result=result, image=image)
+return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
